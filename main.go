55668e79469a5a08a2cebe1e08257c7346b7244c
AEX0TIC <nowherelse619@hotmail.com>
2025-08-10 17:14:18 +0530

    Added the ascii art

--- main.go
+++ main.go
@@ -7,7 +7,17 @@ import (
 	"os"
 )
 
+func printBanner() {
+	fmt.Println(" ____   ____    _    _   _        ___  _   _        ____  ___  ")
+	fmt.Println("/ ___| / ___|  / \  | \ | |      / _ \| \ | |      / ___|/ _ \ ")
+	fmt.Println("\___ \| |     / _ \ |  \| |_____| | | |  \| |_____| |  _| | | |")
+	fmt.Println(" ___) | |___ / ___ \| |\  |_____| |_| | |\  |_____| |_| | |_| |")
+	fmt.Println("|____/ \____/_/   \_\_| \_|      \___/|_| \_|      \____|\___/ ")
+}
+
 func main() {
+	printBanner()
+
 	target := flag.String("target", "", "Target IP or domain")
 	start := flag.Int("start", 1, "Start port")
 	end := flag.Int("end", 1024, "End Port")
